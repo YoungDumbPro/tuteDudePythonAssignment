45497b6d4fdc92afc7db74331985c2a51101dd7d
YoungDumbPro <Nischalssup@gmail.com>
2025-12-05 20:35:35 +0530

    task2 done

---
+++ Assignment1/task2.py
@@ -0,0 +1,7 @@
+print("What is your first name?")
+first_name = input()
+
+print("What is your last name?")
+last_name = input()
+
+print("Hello " + first_name + " " + last_name + "! Nice to meet you.")
