3b373dd39f4a49aaf59127137c78e39faaa7a3ba
YoungDumbPro <Nischalssup@gmail.com>
2025-12-05 20:31:24 +0530

    task one pushed on git

--- Assignment1/task1.py
+++ Assignment1/task1.py
@@ -1,1 +1,30 @@
-print(25)
+# Perform Basic Mathematical Operations
+
+
+# Taking float input from user as they can enter decimal values also
+print("Enter first number: ")
+
+num1 = float(input())
+
+print("Enter second number: ")
+
+num2 = float(input())
+
+# Addition
+sum = num1 + num2
+
+print("Addition: ", sum)
+
+# Subtraction
+difference = num1 - num2       
+print("Subtraction: ", difference)  
+
+# Multiplication
+product = num1 * num2   
+print("Multiplication: ", product)
+
+# Division
+quotient = num1 / num2          
+print("Division: ", quotient)
+
+
