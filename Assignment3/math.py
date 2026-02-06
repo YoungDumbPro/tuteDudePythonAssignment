17e58aab68cf28a74bd365aed16f84c2253dee6c
YoungDumbPro <nischalssup@gmail.com>
2026-02-06 20:05:34 +0530

    Add Assignment3 files

---
+++ Assignment3/math.py
@@ -0,0 +1,8 @@
+import math
+num = int(input("Enter a number: "))
+square_root = math.sqrt(num)
+logarithm = math.log(num)
+sine_value = math.sin(num)
+print(f"The square root of {num} is {square_root}.")
+print(f"The natural logarithm of {num} is {logarithm}.")
+print(f"The sine of {num} is {sine_value}.")
